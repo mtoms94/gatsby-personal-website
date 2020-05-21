cc7f728a57ae05ac22cc46468ad687c113f79139
mtoms94 <mdt1@williams.edu>
2020-05-21 08:53:22 -0400

    probably time to commit something

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /vectors/,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
@@ -15,7 +23,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/vectors/icon.svg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import Icon from "../vectors/initials.svg";
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <Icon className="icon"></Icon>
     </div>
     <div className="content">
       <div className="inner">
